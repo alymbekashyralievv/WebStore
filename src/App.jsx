87c8fa35a4b655557c7053706649402efcaca74a
alymbek <alymbekashyraliev206@gmai.com>
2025-03-02 20:10:39 +0600

    Header finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page/HomePage";
+import DetailPage from "./pages/detail-page/DetailPage";
+import NavBar from "./nav-bar/NavBar";
+import NotFound from "./pages/not-found/NotFound";
+import Header from "./components/header/Header";
 
 const App = () => {
-  return <div className="text-[red]">App</div>;
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { GoHeart } from "react-icons/go";
+import { SlChart } from "react-icons/sl";
+import { HiOutlineUser } from "react-icons/hi";
+import { IoIosArrowDown } from "react-icons/io";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="container">
+      <header className="flex h-[76px] items-center">
+        <div className="flex gap-[10px]">
+          <img className="w-[38px]" src="/src/assets/behoof.svg" alt="" />
+          <h2 className="text-[34px] font-bold">Behoof</h2>
+          <p>
+            Лучшие цены <br /> в интернет-магазинaх
+          </p>
+        </div>
+        <button
+          className="bg-[#f9553c] w-[200px] h-[52px] flex items-center justify-center gap-[10px] ml-[20px] text-white rounded-[10px]"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          Каталог товаров <IoIosArrowDown />
+        </button>
+        {isOpen && (
+          <div>
+            <ul className="">
+              <li className="">Товар 1</li>
+              <li className="">Товар 2</li>
+              <li className="">Товар 3</li>
+            </ul>
+          </div>
+        )}
+
+        <div className="relative flex gap-[10px]">
+          <CiSearch className="absolute left-[7px] top-[14px] text-[25px]" />
+          <input
+            className="h-[52px] rounded-[10px] bg-[#f4efef] pl-[35px] w-[640px]"
+            placeholder="Поиск товаров"
+          />
+          <button className="w-[52px] h-[52px] bg-[#f4efef] rounded-[10px] flex items-center justify-center">
+            <GoHeart />
+          </button>
+          <button className="w-[52px] h-[52px] bg-[#f4efef] rounded-[10px] flex items-center justify-center">
+            <SlChart />
+          </button>
+          <button className="w-[52px] h-[52px] bg-[#f4efef] rounded-[10px] flex items-center justify-center">
+            <HiOutlineUser />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
