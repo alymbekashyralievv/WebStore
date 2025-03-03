d67fbdfbde1acaee9f74f39eb819025f9b8ca4e5
Anelya92008 <alelya.satybaldieva@gmail.com>
2025-03-03 10:48:42 +0300

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page/HomePage";
+import DetailPage from "./pages/detail-page/DetailPage";
+import NavBar from "./nav-bar/NavBar";
+import NotFound from "./pages/not-found/NotFound";
+import Header from "./components/Header"
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="text-[red]">App</div>;
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Header />
+        <Footer/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
